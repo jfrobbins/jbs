55106e0de33f79206abe77e94cdb39b20326ee5b
Jon Robbins <jfrobbins@gmail.com>
2011-01-20 22:24:22 -0500

    nothing to see here

--- config.php
+++ config.php
@@ -101,8 +101,8 @@
     $jbs_formconfig .= "\n" . '<script type="text/javascript">
                                 /* <![CDATA[ */
                                     (function() {
-                                        var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
-                                        s.type = 'text/javascript';
+                                        var s = document.createElement("script"), t = document.getElementsByTagName("script")[0];
+                                        s.type = "text/javascript";
                                         s.async = true;
                                         s.src = "http://api.flattr.com/js/0.6/load.js?mode=auto";
                                         t.parentNode.insertBefore(s, t);
